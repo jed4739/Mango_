5bc52355f77c792c1f899b43ddf2e810b2dff8bd
jed4739 <tjsdlr04@gmail.com>
2022-12-13 09:17:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mango_/JoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.mango_;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/example/mango_/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mango_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
