9c68c3aa57efcfdb918033f2ce5a323a766f6746
jed4739 <tjsdlr04@gmail.com>
2022-12-29 10:11:21 +0900

    지옥도 2022.12.29 -1

--- app/src/main/java/com/example/mango_/SplashActivity.java
+++ app/src/main/java/com/example/mango_/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.mango_;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,6 +13,9 @@ import androidx.databinding.DataBindingUtil;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     @Override
@@ -24,23 +28,17 @@ public class SplashActivity extends AppCompatActivity {
 
         try {
             Log.i("app_test","SplashActivity.class, " + "try getUid -> MainActivity.class");
-            auth.getCurrentUser().getUid();
-            moveMain(MainActivity.class);
+            Objects.requireNonNull(auth.getCurrentUser()).getUid();
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }, 3000L);
         } catch (NullPointerException e) {
             Log.i("app_test","SplashActivity.class, " + "catch NullPointException -> JoinActivity.class");
-            moveMain(JoinActivity.class);
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                startActivity(new Intent(getApplicationContext(), JoinActivity.class));
+                finish();
+            }, 3000L);
         }
-    }
-    /*
-     * SPLASH TIME METHOD
-     * Splash.java -> MainActivity.java
-     * 단위 : 1초 (1000L)
-     * */
-    private void moveMain(Class clazz) {
-        Log.i("app_test","SplashActivity.class, " + "Splash Handler");
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            startActivity(new Intent(getApplicationContext(), clazz));
-            finish();
-        }, 3000L);
     }
 }
